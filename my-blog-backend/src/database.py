70282ad0fed33566a992f501b2a24b6628750bbf
noahal1 <noahall127@outlook.com>
2025-04-10 08:49:50 +0800

    部署后更新bug

--- my-blog-backend/src/database.py
+++ my-blog-backend/src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://noah:727127zb@noahall.chat:3306/blog"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:727127zb@60.205.12.17:3306/myblog"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
